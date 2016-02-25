5de6fc624d594790476e949f3529f53217e4e992
Grant <grantzukowski@gmail.com>
2016-02-24 19:12:05 -0800

    this is a test

---
+++ src/apples.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class apples {
+	public static void main(String[] args){
+		
+		createfile g = new createfile();
+		g.openFile();
+		g.addRecords();
+		g.closeFile();
+		
+	}
+}
